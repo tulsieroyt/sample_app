b7be8c332d1a5c1f99d048a3626bff958f0858a3
Tulsie Chandra Barman <tulsieroyt@gmail.com>
2024-06-17 13:37:48 +0600

    Home screen and SplashScreen are added

---
+++ authentication_app/lib/src/screens/splash_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Spacer(),
+          Image.asset('assets/logo/auth_logo.png'),
+          const Spacer(),
+          const CircularProgressIndicator(),
+          const SizedBox(height: 50)
+        ],
+      ),
+    );
+  }
+}
